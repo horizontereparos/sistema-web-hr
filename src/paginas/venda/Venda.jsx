b6151d95ad65df6921a85bbe79e641d19513f5d3
Raphael <raphaelrodrigues93@gmail.com>
2022-08-24 12:22:53 -0300

    ajuste no alerta de desenvolvimento

--- src/paginas/venda/Venda.jsx
+++ src/paginas/venda/Venda.jsx
@@ -1,10 +1,14 @@
 import alerta from '../../assets/img/alerta-desenvolvimento.png'
+import Cabecalho from '../../components/cabecalho/Cabecalho'
 import './venda.css'
 export default function Venda(){
     return (
-        <div className="alerta-desenvolvimento">
-            <h1 className='texto-alerta'>Pagina em desenvolvimento</h1>
-            <img className='imagem-alerta' src={alerta} />
+        <div>
+            <Cabecalho className='cabecalho-alerta'>Alerta</Cabecalho>
+            <div className="alerta-desenvolvimento">
+                <h1 className='texto-alerta'>Pagina em desenvolvimento</h1>
+                <img className='imagem-alerta' src={alerta} />
+            </div>
         </div>
     )
 }
